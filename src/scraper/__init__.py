e969c3e429c8f0c2a6c2c47ecc4c1ee563994d74
Ian Hoyle <ian@ianhoyle.com>
2026-02-01 13:42:35 -0600

    Delete

--- src/scraper/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Scraper module - discovers Wikimedia dumps."""
-
-from src.scraper.wikimedia import WikimediaClient
-from src.scraper.worker import ScraperWorker
-
-__all__ = ["ScraperWorker", "WikimediaClient"]
